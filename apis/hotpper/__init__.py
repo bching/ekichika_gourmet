c05db5a8a34766d94e64cdbc9068c1f70f4cb654
umihico <umihico_dummy@users.noreply.github.com>
2018-11-22 17:40:46 +0900

    whole system tree with empty functions

---
+++ apis/hotpper/__init__.py
@@ -0,0 +1,5 @@
+def hotpper_api(y, x):
+    """
+    (dummy till metro.py is complete)
+    y is around 35 and x is around 135.
+    """
---
+++ apis/test_api.py
@@ -0,0 +1,14 @@
+from metro import metro_api
+from hotpper import hotpper_api
+
+"""
+目黒駅 is @35.6340074,139.7135059
+args are named 'y' and 'x' ('y' comes first)
+(https://www.google.com/maps/search/%E7%9B%AE%E9%BB%92%E9%A7%85/@35.6340074,139.7135059,17z/data=!3m1!4b1)
+"""
+
+if __name__ == '__main__':
+    """args must be float"""
+    import pprint
+    pprint.pprint(metro_api(35.6340074, 139.7135059))
+    pprint.pprint(hotpper_api(35.6340074, 139.7135059))
